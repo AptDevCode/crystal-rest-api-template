dfd60913e9eaaba64ea87808838835b33310db4a
Jose Chavez <jose.e.chavez@gmail.com>
2023-07-08 20:48:53 -0700

    crystal init

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/crystal-rest-api-template"
---
+++ src/crystal-rest-api-template.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Crystal::Rest::Api::Template`
+module Crystal::Rest::Api::Template
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
